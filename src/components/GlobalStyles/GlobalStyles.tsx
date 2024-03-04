2e343e67de63ee29502bb46c27b8353a5b2ce042
ThanhDatTu3tle <thanhdat11badinh@gmail.com>
2024-03-04 15:14:24 +0700

    #2 - feat(styles UI): add GlobalStyles component & reset css file

---
+++ src/components/GlobalStyles/GlobalStyles.tsx
@@ -0,0 +1,9 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import React from 'react'
+import './GlobalStyles.module.scss'
+
+const GlobalStyles: React.FC<any> = ({ children }) => {
+  return children
+}
+
+export default GlobalStyles
