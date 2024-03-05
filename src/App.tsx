bf2852812b22b269e548fd811da908eb0bb149ab
ThanhDatTu3tle <thanhdat11badinh@gmail.com>
2024-03-05 12:00:42 +0700

    #10 - feat(routes): apply routes for the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,68 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import React from 'react'
+import { Fragment } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
-function App() {
-  const [count, setCount] = useState(0)
+import { publicRoutes } from './routes'
 
+import { UserLayout } from './layouts/UserLayout'
+
+const App: React.FC<any> = () => {
   return (
     <>
-      <div>
-        <a href='https://vitejs.dev' target='_blank'>
-          <img src={viteLogo} className='logo' alt='Vite logo' />
-        </a>
-        <a href='https://react.dev' target='_blank'>
-          <img src={reactLogo} className='logo react' alt='React logo' />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className='card'>
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className='read-the-docs'>Click on the Vite and React logos to learn more</p>
+      <Router>
+        <div className='App'>
+          <Routes>
+            {publicRoutes.map((route, index) => {
+              const Page = route.component
+
+              let Layout = UserLayout
+
+              if (route.layout) {
+                Layout = route.layout
+              } else if (route.layout === null) {
+                Layout = Fragment
+              }
+              return (
+                <Route
+                  key={index}
+                  path={route.path}
+                  element={
+                    <Layout>
+                      <Page />
+                    </Layout>
+                  }
+                />
+              )
+            })}
+            {/* {privateRoutes.map((route, index) => {
+            const Page = route.component
+
+            let Layout = MainAdminLayout
+
+            if (route.layout) {
+              Layout = route.layout
+            } else if (route.layout === null) {
+              Layout = Fragment
+            }
+            return (
+              <Route
+                key={index}
+                path={route.path}
+                element={
+                  <ProtectedRoute>
+                    <Layout>
+                      <Page />
+                    </Layout>
+                  </ProtectedRoute>
+                }
+              />
+            )
+          })} */}
+          </Routes>
+        </div>
+      </Router>
     </>
   )
 }
--- src/components/GlobalStyles/GlobalStyles.tsx
+++ src/components/GlobalStyles/GlobalStyles.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import React from 'react'
-import './GlobalStyles.module.scss'
+import './GlobalStyles.module.css'
 
 const GlobalStyles: React.FC<any> = ({ children }) => {
   return children
