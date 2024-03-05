567dc9c53a880e5dea42e1359cb48d90840e3cfa
ThanhDatTu3tle <thanhdat11badinh@gmail.com>
2024-03-05 10:21:19 +0700

    #1 - feat(config): config route

--- src/config/routes.ts
+++ src/config/routes.ts
@@ -1,31 +1,31 @@
 const routes = {
-  home: '/',
-  introduce: '/gioi-thieu',
-  discount: '/khuyen-mai',
-  news: '/tin-tuc',
-  contact: '/lien-he',
-  login: '/login',
-  cart: '/cart',
+  home: '/'
+  // introduce: '/gioi-thieu',
+  // discount: '/khuyen-mai',
+  // news: '/tin-tuc',
+  // contact: '/lien-he',
+  // login: '/login',
+  // cart: '/cart',
 
-  detailCategory: '/detailCategory/:name',
-  detailProduct: '/detailProduct/:id',
+  // detailCategory: '/detailCategory/:name',
+  // detailProduct: '/detailProduct/:id',
 
-  privacyPolicy: '/chinh-sach-bao-mat',
-  shoppingGuide: '/huong-dan-mua-hang',
-  paymentGuide: '/chinh-sach-thanh-toan',
-  shippingPolicy: '/chinh-sach-van-chuyen',
-  refund: '/chinh-sach-hoan-tien',
+  // privacyPolicy: '/chinh-sach-bao-mat',
+  // shoppingGuide: '/huong-dan-mua-hang',
+  // paymentGuide: '/chinh-sach-thanh-toan',
+  // shippingPolicy: '/chinh-sach-van-chuyen',
+  // refund: '/chinh-sach-hoan-tien',
 
-  admin: '/admin',
-  productManagement: '/management',
-  productDiscount: '/discount',
-  category: '/category',
-  brand: '/brand',
-  invoice: '/invoice',
-  bill: '/bill',
-  user: '/user',
-  combo: '/combo',
-  event: '/event'
+  // admin: '/admin',
+  // productManagement: '/management',
+  // productDiscount: '/discount',
+  // category: '/category',
+  // brand: '/brand',
+  // invoice: '/invoice',
+  // bill: '/bill',
+  // user: '/user',
+  // combo: '/combo',
+  // event: '/event'
 }
 
 export default routes
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,26 +1,26 @@
 // routes.ts
-// import config from '../config'
+import config from '../config'
 
-// // Layouts
+// Layouts
+import { UserLayout } from '~/layouts/UserLayout'
 
-// // Pages
+// Pages
+import Home from '~/pages/Home'
 
 // // Admin Page
 
-// const publicRoutes = [
-//   //   { path: config.routes.home, component: Home, layout: MainUserLayout },
-//   //   { path: config.routes.introduce, component: Introduce, layout: MainUserLayout },
-//   //   { path: config.routes.discount, component: Discount, layout: MainUserLayout },
-//   //   { path: config.routes.news, component: News, layout: MainUserLayout },
-//   //   { path: config.routes.contact, component: Contact, layout: MainUserLayout },
-//   //   { path: config.routes.detailCategory, component: DetailCategory, layout: MainUserLayout },
-//   //   { path: config.routes.detailProduct, component: DetailProduct, layout: MainUserLayout },
-//   //   { path: config.routes.cart, component: Cart, layout: MainUserLayout },
-//   //   { path: config.routes.login, component: LoginAdmin, layout: null }
-// ]
+const publicRoutes = [
+  { path: config.routes.home, component: Home, layout: UserLayout }
+  //   { path: config.routes.introduce, component: Introduce, layout: MainUserLayout },
+  //   { path: config.routes.discount, component: Discount, layout: MainUserLayout },
+  //   { path: config.routes.news, component: News, layout: MainUserLayout },
+  //   { path: config.routes.contact, component: Contact, layout: MainUserLayout },
+  //   { path: config.routes.detailCategory, component: DetailCategory, layout: MainUserLayout },
+  //   { path: config.routes.detailProduct, component: DetailProduct, layout: MainUserLayout },
+  //   { path: config.routes.cart, component: Cart, layout: MainUserLayout },
+  //   { path: config.routes.login, component: LoginAdmin, layout: null }
+]
 
-// //   const privateRoutes = [
+// const privateRoutes = []
 
-// //   ];
-
-// export { publicRoutes, privateRoutes }
+export { publicRoutes }
